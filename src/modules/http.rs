0fede60f23327f239e86c44223e18a1372f00a2a
letheriver2007 <letheriver2007@126.com>
2024-04-30 01:21:43 +0800

    [Add]:Support Custom Server Url

--- src/modules/http.rs
+++ src/modules/http.rs
@@ -5,6 +5,16 @@ use crate::marshal;
 use anyhow::Result;
 use ilhook::x64::Registers;
 
+use std::fs;
+
+fn read_server_txt() -> Result<String, std::io::Error> {
+    fs::read_to_string("server.txt")
+}
+
+fn write_default_url_to_server_txt() -> Result<(), std::io::Error> {
+    fs::write("server.txt", "http://127.0.0.1:443")
+}
+
 const UNITY_WEB_REQUEST_SET_URL: usize = 0x0D9442D0;
 
 pub struct Http;
@@ -33,7 +43,17 @@ unsafe extern "win64" fn on_uwr_set_url(reg: *mut Registers, _: usize) {
     let slice = std::slice::from_raw_parts(str_ptr, (str_length * 2) as usize);
     let url = String::from_utf16le(slice).unwrap();
 
-    let mut new_url = String::from("http://127.0.0.1:2024");
+    let mut new_url = match read_server_txt() {
+        Ok(content) => content,
+        Err(_) => {
+            if let Err(err) = write_default_url_to_server_txt() {
+                println!("Failed to create server.txt: {}", err);
+            }
+            println!("Cound not find server.txt, created it automatically!");
+            String::from("http://127.0.0.1:443")
+        },
+    };
+
     url.split('/').skip(3).for_each(|s| {
         new_url.push_str("/");
         new_url.push_str(s);
